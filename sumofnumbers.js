9f5f347d5640a715cad21a6c7c3b8dc8f1117c69
ThermoDev <michael.pegios@hotmail.com>
2020-04-19 00:11:19 +1000

    Added Running Times

--- sumofnumbers.js
+++ sumofnumbers.js
@@ -1,5 +1,5 @@
 // Global list 
-var listOfNums = [1, 2, 4, 12, 32];
+var listOfNums = Array.from({length: 500}, () => Math.floor(Math.random() * 100));
 
 
 function sumFor() {
@@ -31,8 +31,9 @@ function sumRecursion() {
     return count;
 }
 
+// Called by sumRecursion()
 function sumRecursionHelper(myList) {
-    if (myList.length == 0) {
+    if (myList == undefined || myList.length == 0 ) {
         return 0;
     }
     return myList[0] + sumRecursionHelper(myList.slice(1))
@@ -44,3 +45,27 @@ function sumTheSimpleWay() {
         return memo + num;
     });
 }
+window.onload = function(){
+    var t0Before = performance.now()
+    var result = sumFor();
+    var t0After = performance.now();
+    document.getElementById("sumFor").innerHTML = "<b>Sum For Loop: </b>" + (t0After - t0Before);
+
+    var t1Before = performance.now()
+    var result = sumWhile();
+    var t1After = performance.now()
+    document.getElementById("sumWhile").innerHTML = "<b>Sum While Loop: </b>" +  (t1After - t1Before);
+
+    var t2Before = performance.now()
+    var result = sumRecursion();
+    var t2After = performance.now()
+    document.getElementById("sumRecursion").innerHTML = "<b>Sum Recursion: </b>" + (t2After - t2Before);
+
+    var t3Before = performance.now()
+    var result = sumTheSimpleWay();
+    var t3After = performance.now()
+    document.getElementById("sumSimple").innerHTML = "<b>Sum Underscore JS: </b>" + (t3After - t3Before);
+  };
+
+
+
